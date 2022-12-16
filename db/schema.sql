09f764be34cae2612fd24de20209042f39dcd487
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-16 08:12:48 -0600

    Added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    departmentName VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id),
+    REFERENCES employeeRole(id)
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id),
+    ON DELETE SET NULL,
+);
